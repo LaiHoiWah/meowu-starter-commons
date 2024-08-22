613f85f654ca07b8f65ca37b24ea55a45dccce9a
Lihaihua <583268519@qq.com>
2024-08-22 09:14:30 +0800

    <code> modify utils

--- src/main/java/com/meowu/starter/commons/utils/ObjectUtils.java
+++ src/main/java/com/meowu/starter/commons/utils/ObjectUtils.java
@@ -1,8 +1,5 @@
 package com.meowu.starter.commons.utils;
 
-/**
- * Suggest using this util instead of org.apache.commons.lang3.ObjectUtils
- */
 public class ObjectUtils{
 
     private ObjectUtils(){
@@ -15,17 +12,6 @@ public class ObjectUtils{
 
     public static boolean isNotNull(Object o){
         return !isNull(o);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> T deepClone(T source){
-        AssertionUtils.notNull(source, "Source object must not be null");
-        // get class
-        Class<?> classOf = source.getClass();
-        // to json
-        String json = GsonUtils.toJson(source);
-        // to object
-        return (T) GsonUtils.fromJson(json, classOf);
     }
 
     public static String toString(Object o){
